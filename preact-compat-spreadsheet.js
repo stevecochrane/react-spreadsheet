3f297da0baa607a11c18d05cfa62b939bea61bb4
Steve Cochrane <steve@stevecochrane.com>
2017-09-04 14:38:52 -0500

    Added a basic application JS file and a start script to run it.

---
+++ preact-compat-spreadsheet.js
@@ -0,0 +1,25 @@
+const express     = require("express");
+
+//	Initialize Express
+const app = express();
+
+//	Default to port 3000
+app.set("port", process.env.PORT || 3000);
+
+//	Set /views as the default views directory
+app.set("views", "./views");
+
+//	Set Pug as the default template engine
+app.set("view engine", "pug");
+
+//	Set contents of /public to be served as static files with "/static/" URL prefix
+app.use("/static", express.static(__dirname + "/public"));
+
+//	Default app view
+app.get("/", function(req, res) {
+	res.render("index");
+});
+
+app.listen(app.get("port"), function() {
+	console.log("Express started on http://localhost:" + app.get("port") + "; press Ctrl-C to terminate.");
+});
