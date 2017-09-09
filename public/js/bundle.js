d6171eb14b0a4ade2be75c4dc808484f0fb28171
Steve Cochrane <steve@stevecochrane.com>
2017-09-09 17:36:08 -0500

    Ran a fresh build for recent changes.

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -9758,9 +9758,9 @@ var _reactDom = __webpack_require__(98);
 
 var _reactDom2 = _interopRequireDefault(_reactDom);
 
-var _Excel = __webpack_require__(184);
+var _Spreadsheet = __webpack_require__(184);
 
-var _Excel2 = _interopRequireDefault(_Excel);
+var _Spreadsheet2 = _interopRequireDefault(_Spreadsheet);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
@@ -9769,7 +9769,7 @@ var headers = ["Book", "Author", "Language", "Published", "Sales"];
 var data = [["The Lord of the Rings", "J. R. R. Tolkien", "English", "1954-1955", "150 million"], ["Le Petit Prince (The Little Prince)", "Antoine de Saint-Exupery", "French", "1943", "140 million"], ["Harry Potter and the Philosopher's Stone", "J. K. Rowling", "English", "1997", "107 million"], ["And Then There Were None", "Agatha Christie", "English", "1939", "100 million"], ["Dream of the Red Chamber", "Cao Xueqin", "Chinese", "1754-1791", "100 million"], ["The Hobbit", "J. R. R. Tolkien", "English", "1937", "100 million"], ["She: A History of Adventure", "H. Rider Haggard", "English", "1887", "100 million"]];
 
 document.addEventListener("DOMContentLoaded", function (event) {
-    _reactDom2.default.render(_react2.default.createElement(_Excel2.default, { headers: headers, initialData: data }), document.getElementById("excel"));
+	_reactDom2.default.render(_react2.default.createElement(_Spreadsheet2.default, { headers: headers, initialData: data }), document.getElementById("spreadsheet"));
 });
 
 /***/ }),
@@ -22423,13 +22423,13 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var Excel = function (_React$Component) {
-	_inherits(Excel, _React$Component);
+var Spreadsheet = function (_React$Component) {
+	_inherits(Spreadsheet, _React$Component);
 
-	function Excel(props) {
-		_classCallCheck(this, Excel);
+	function Spreadsheet(props) {
+		_classCallCheck(this, Spreadsheet);
 
-		var _this = _possibleConstructorReturn(this, (Excel.__proto__ || Object.getPrototypeOf(Excel)).call(this, props));
+		var _this = _possibleConstructorReturn(this, (Spreadsheet.__proto__ || Object.getPrototypeOf(Spreadsheet)).call(this, props));
 
 		_this._toggleSearch = _this._toggleSearch.bind(_this);
 		_this._search = _this._search.bind(_this);
@@ -22447,7 +22447,7 @@ var Excel = function (_React$Component) {
 		return _this;
 	}
 
-	_createClass(Excel, [{
+	_createClass(Spreadsheet, [{
 		key: "render",
 		value: function render() {
 			return _react2.default.createElement(
@@ -22611,10 +22611,10 @@ var Excel = function (_React$Component) {
 		}
 	}]);
 
-	return Excel;
+	return Spreadsheet;
 }(_react2.default.Component);
 
-exports.default = Excel;
+exports.default = Spreadsheet;
 
 /***/ })
 /******/ ]);
